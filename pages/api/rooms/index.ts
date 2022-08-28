77402f35b5cd0449269064e8df2497550778ed7c
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-08-28 20:36:20 +0300

    Connected error formatting middleware to api/rooms route

--- pages/api/rooms/index.ts
+++ pages/api/rooms/index.ts
@@ -1,7 +1,8 @@
 import nc from "next-connect";
 import { allRooms, createRoom } from "../../../controllers/controllers";
+import onError from "../../../middleware/errorFormattingMiddleware";
 
-const handler = nc();
+const handler = nc({ onError });
 
 handler.get(allRooms);
 handler.post(createRoom);
