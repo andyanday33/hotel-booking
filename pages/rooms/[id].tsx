9b96cbb6981e6d91db794c76ee720bb393695b90
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-09-02 22:30:27 +0300

    Update TODOs

--- pages/rooms/[id].tsx
+++ pages/rooms/[id].tsx
@@ -15,7 +15,8 @@ const TrpcTest: NextPage = (props) => {
   const { data: room, error } = trpc.useQuery(["getSingleRoom", { id: +id }]);
   return (
     <>
-      {/* TODO: consider extracting layout */}
+      {/* TODO: consider extracting layout
+      TODO: pass down data as a prop if not initial request */}
       <Header />
       <main className="min-w-full min-h-screen bg-gray-800">
         <ul>
