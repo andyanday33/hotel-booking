5f0c11a80202aa1d626f01d8f6a35e73dc9e9408
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-09-03 16:11:27 +0300

    Further styling updates

--- pages/rooms/index.tsx
+++ pages/rooms/index.tsx
@@ -8,7 +8,7 @@ const TrpcTest: NextPage = (props) => {
   const { data, error } = trpc.useQuery(["room.getAllRooms", { name: "" }]);
   return (
     <Layout>
-      <section className="grid gap-3 pt-3 mt-3 xs:grid-cols-1 xs:grid-rows-6 md:grid-cols-2 md:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 justify-items-center">
+      <section className="grid gap-3 my-6 xs:grid-cols-1 xs:grid-rows-6 md:grid-cols-2 md:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 justify-items-center">
         {/* TODO: add a spinner here */}
         {!data && !error && <p>Loading</p>}
         {error && <p>Error: {error.message}</p>}
