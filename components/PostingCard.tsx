0c934f76a48d42b25d236a3e9b3ee67f44afaf43
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-09-04 21:46:09 +0300

    chore: posting card styling updates

--- components/PostingCard.tsx
+++ components/PostingCard.tsx
@@ -14,7 +14,7 @@ const PostingCard: React.FC<Props> = ({ room }) => {
     <Link href={`/rooms/${room.id}`}>
       <a className="">
         <div className="card border-2 border-gray-600 bg-base-100 h-full shadow-xl motion-safe:hover:scale-105 duration-500">
-          <div className="container min-h-[30%] max-h-[60%] xs:h-[40%]">
+          <div className="container min-h-[30%] max-h-[60%] md:h-[40%]">
             <figure className="flex h-full">
               {/* There are images for this room */}
               {room.images && room.images[0] && (
@@ -45,11 +45,12 @@ const PostingCard: React.FC<Props> = ({ room }) => {
           </div>
 
           <div className="flex card-body">
-            <h2 className="card-title text-sm h-[35%]">
-              {room.name}
+            <h2 className="card-title text-gray-300 text-sm min-h-[35%] md:text-xs">
+              <strong>{room.name}</strong>
               <div className="badge badge-secondary">NEW</div>
             </h2>
-            <section className="h-1/6 price mt-2 text-sm">
+            <section className="min-h-1/6 price text-sm">
+              <p className="text-xs">{room.address}</p>
               <p>
                 <span>
                   <strong>£{room.pricePerNight.toFixed(2)}</strong>
@@ -63,7 +64,7 @@ const PostingCard: React.FC<Props> = ({ room }) => {
               numberOfReviews={room.numOfReviews}
               readOnly={true}
             />
-            <div className="h-[1rem] xs:h-[3rem] card-actions justify-end">
+            <div className="min-h-[0.5rem] xs:min-h-[1rem] card-actions justify-end">
               {/* TODO: add other badges here */}
               {room.internet && (
                 <div className="badge badge-outline text-xs">Internet</div>
