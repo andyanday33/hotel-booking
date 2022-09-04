1488590bdd4545d71ae0a7f19087ad78e5bff62f
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-09-04 12:25:22 +0300

    rooms route markup updates

--- pages/rooms/index.tsx
+++ pages/rooms/index.tsx
@@ -8,6 +8,9 @@ const TrpcTest: NextPage = (props) => {
   const { data, error } = trpc.useQuery(["room.getAllRooms", { name: "" }]);
   return (
     <Layout>
+      <h2 className="text-center mt-4 text-2xl">
+        All Hotel and House Postings
+      </h2>
       <section className="grid gap-6 my-6 xs:grid-cols-1 xs:grid-rows-6 md:grid-cols-2 md:grid-rows-3 lg:grid-cols-3 lg:grid-rows-2 justify-items-center">
         {/* TODO: add a spinner here */}
         {!data && !error && <p>Loading</p>}
