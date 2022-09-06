8afc1f75fbc49b73840097363d7f18759da849f5
Berke Anday Baloğlu <andyanday33@gmail.com>
2022-09-06 22:22:20 +0300

    chore: component name and logic separation

--- pages/rooms/[id].tsx
+++ pages/rooms/[id].tsx
@@ -3,18 +3,82 @@ import { trpc } from "../../utils/trpc";
 import { useRouter } from "next/router";
 import Layout from "../../components/layout/Layout";
 import ImageWithFallback from "../../components/ImageWithFallback";
+import { Room, RoomImage } from "@prisma/client";
 
-type roomFeatureProps = {
+type FeatureProps = {
   feature: string;
   exists: boolean;
 };
-const FeatureIndicator: React.FC<roomFeatureProps> = ({ feature, exists }) => (
+
+type RoomDetalsProps = {
+  room: Room & { images: RoomImage[] };
+};
+
+const FeatureIndicator: React.FC<FeatureProps> = ({ feature, exists }) => (
   <li>
     {feature}: {exists ? <span>&#10003;</span> : <span>&#10060;</span>}
   </li>
 );
 
-const TrpcTest: NextPage = (props) => {
+const RoomDetails: React.FC<RoomDetalsProps> = ({ room }) => {
+  return (
+    <>
+      <section
+        className="room-details text-center sm:text-start row-[2] mx-4 sm:mx-0 sm:max-w-[50%] mr-4
+  divide-y divide-gray-700"
+      >
+        <div className="room-header pb-4">
+          <h2 className="text-4xl mb-2">{room.name}</h2>
+          <p className="text-sm mb-4">
+            &#9733; {room.ratings.toFixed(2)} | {room.numOfReviews} reviews |{" "}
+            {room.address}
+          </p>
+          {/* TODO: Configure user queries on tRPC */}
+          {room.creatorId && <p>Hosted by {room.creatorId}</p>}
+          <p>{room.description}</p>
+        </div>
+        <div className="room-features pt-4">
+          <h2 className="text-xl mb-4">Room Features</h2>
+          <div className="container divide-x divide-gray-700 grid grid-cols-2">
+            <ul>
+              <FeatureIndicator feature="Breakfast" exists={room.breakfast} />
+              <FeatureIndicator feature="Internet" exists={room.internet} />
+              <FeatureIndicator
+                feature="Pets Allowed"
+                exists={room.petsAllowed}
+              />
+              <FeatureIndicator
+                feature="Room Cleaning"
+                exists={room.roomCleaning}
+              />
+              <FeatureIndicator
+                feature="Air Conditioned"
+                exists={room.airconditioned}
+              />
+            </ul>
+            <ul className="pl-2">
+              <li>Number of Beds: {room.numOfBeds}</li>
+              <li>
+                Room Category:{" "}
+                {room.category.charAt(0) + room.category.slice(1).toLowerCase()}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <ImageWithFallback
+        src={room?.images[0]?.url}
+        fallBackSrc={"/placeholder.jpeg"}
+        layout="intrinsic"
+        width={1000}
+        height={1000}
+      />
+    </>
+  );
+};
+
+const SingleRoom: NextPage = (props) => {
   const router = useRouter();
   const { id } = router.query;
 
@@ -41,66 +105,7 @@ const TrpcTest: NextPage = (props) => {
           {/* TODO: add a spinner here */}
           {!room && !error && <p>Loading</p>}
           {error && <p>Error: {error.message}</p>}
-          {room && (
-            <>
-              <section
-                className="room-details text-center sm:text-start row-[2] mx-4 sm:mx-0 sm:max-w-[50%] mr-4
-              divide-y divide-gray-700"
-              >
-                <div className="room-header pb-4">
-                  <h2 className="text-4xl mb-2">{room.name}</h2>
-                  <p className="text-sm mb-4">
-                    &#9733; {room.ratings.toFixed(2)} | {room.numOfReviews}{" "}
-                    reviews | {room.address}
-                  </p>
-                  <p>{room.description}</p>
-                </div>
-                <div className="room-features pt-4">
-                  <h2 className="text-xl mb-4">Room Features</h2>
-                  <div className="container divide-x divide-gray-700 grid grid-cols-2">
-                    <ul>
-                      <FeatureIndicator
-                        feature="Breakfast"
-                        exists={room.breakfast}
-                      />
-                      <FeatureIndicator
-                        feature="Internet"
-                        exists={room.internet}
-                      />
-                      <FeatureIndicator
-                        feature="Pets Allowed"
-                        exists={room.petsAllowed}
-                      />
-                      <FeatureIndicator
-                        feature="Room Cleaning"
-                        exists={room.roomCleaning}
-                      />
-                      <FeatureIndicator
-                        feature="Air Conditioned"
-                        exists={room.airconditioned}
-                      />
-                    </ul>
-                    <ul className="pl-2">
-                      <li>Number of Beds: {room.numOfBeds}</li>
-                      <li>
-                        Room Category:{" "}
-                        {room.category.charAt(0) +
-                          room.category.slice(1).toLowerCase()}
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </section>
-
-              <ImageWithFallback
-                src={room?.images[0]?.url}
-                fallBackSrc={"/placeholder.jpeg"}
-                layout="intrinsic"
-                width={1000}
-                height={1000}
-              />
-            </>
-          )}
+          {room && <RoomDetails room={room} />}
         </section>
         <section></section>
       </div>
@@ -108,4 +113,4 @@ const TrpcTest: NextPage = (props) => {
   );
 };
 
-export default TrpcTest;
+export default SingleRoom;
--- pages/rooms/index.tsx
+++ pages/rooms/index.tsx
@@ -4,7 +4,7 @@ import { trpc } from "../../utils/trpc";
 import Layout from "../../components/layout/Layout";
 import PostingCard from "../../components/PostingCard";
 
-const TrpcTest: NextPage = (props) => {
+const Rooms: NextPage = (props) => {
   const { data, error } = trpc.useQuery(["room.getAllRooms", { name: "" }]);
   return (
     <Layout>
@@ -14,11 +14,11 @@ const TrpcTest: NextPage = (props) => {
       <section className="mb-16 mx-[10%] grid grid-cols-1 gap-6 xs:mx-[5%] xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {/* TODO: add a spinner here */}
         {!data && !error && <p>Loading</p>}
-        {error && <p>Error: {error.message}</p>}
+        {!data && error && <p>Error: {error.message}</p>}
         {data && data.map((room) => <PostingCard key={room.id} room={room} />)}
       </section>
     </Layout>
   );
 };
 
-export default TrpcTest;
+export default Rooms;
